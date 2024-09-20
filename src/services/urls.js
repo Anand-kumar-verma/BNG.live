6fd109e56a9f1662ed7f813b71ba71994ec7f3a2
sushmasingh2000 <sushmasingh16166@gmail.com>
2024-09-20 10:22:59 +0530

    jhj

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,6 @@
 export const rupees = "₹";
 // https://admin.bngwin.live/
-export const baseUrl = "http://192.168.18.183:9000";
+export const baseUrl = "https://api.bngwin.live";
 export const fron_end_main_domain = "https://bngwin.live";
 
 export const newdomain1 = "https://api.bngwin.live";
